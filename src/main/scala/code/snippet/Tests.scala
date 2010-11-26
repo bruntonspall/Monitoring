d0f6aa6d1aa2b6f20f2e1a4e315a6bc3ba394fe2
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-11-26 18:06:32 +0000

    Use a wrapping class

--- src/main/scala/code/snippet/Tests.scala
+++ src/main/scala/code/snippet/Tests.scala
@@ -5,22 +5,34 @@ import Helpers._
 import com.google.appengine.api.datastore.FetchOptions.Builder.withDefaults
 import scala.collection.JavaConversions._
 import com.google.appengine.api.datastore.{Entity, FetchOptions, Query, DatastoreServiceFactory}
+import com.google.appengine.api.datastore.Query.FilterOperator
 
+
+class Test(e: Entity) {
+
+  def requestId = e.getProperty("requestId").toString
+//  def requestId_=(s: String) = e.setProperty("requestId", s)
+  def xmlUrl = e.getProperty("xmlUrl").toString
+  def summaryCSV = e.getProperty("summaryCSV").toString
+  def detailsCSV = e.getProperty("detailsCSV").toString
+  def rawXml = e.getProperty("rawXml").toString
+  def ready = e.getProperty("ready") match { case "0" => false; case "1" => true}  
+}
 
 class Tests {
-  private def allTests:List[Entity] = {
-    var query = new Query("Test")
+  private def allTests:List[Test] = {
+    val query = new Query("Test")
     query.addSort("requestId")
 
     val datastore = DatastoreServiceFactory.getDatastoreService
-    datastore.prepare(query).asIterator.toList
+    datastore.prepare(query).asIterator.toList.map(new Test(_))
 
   }
+
   def all= {
     "li" #> allTests.map(test =>
-      "a [href]" #> test.getProperty("requestId").toString &
-      "a *" #> test.getProperty("requestId").toString
+      "a [href]" #> test.requestId &
+      "a *" #> test.requestId
      )
-    //"li" #> allTests.map(test => <a href={"/"+test.getProperty("requestId")}>{test.getProperty("requestId")}</a>)
   }
 }
