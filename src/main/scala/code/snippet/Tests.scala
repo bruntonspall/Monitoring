a512d6455c53187daef7db69f634c3605395cb26
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-12-02 21:00:07 +0000

    Show stats on the frontend

--- src/main/scala/code/snippet/Tests.scala
+++ src/main/scala/code/snippet/Tests.scala
@@ -3,6 +3,7 @@ import _root_.net.liftweb.util._
 import Helpers._
 import scala.collection.JavaConversions._
 import com.google.appengine.api.datastore._
+import com.google.appengine.api.datastore.Query.FilterOperator
 
 
 class Test(e: Entity) {
@@ -14,6 +15,23 @@ class Test(e: Entity) {
   def detailsCSV = e.getProperty("detailsCSV").toString
   def rawXml = e.getProperty("rawXml").asInstanceOf[Text].getValue
   def ready = e.getProperty("ready") match { case "0" => false; case "1" => true}
+  def runs = DatastoreServiceFactory.getDatastoreService.prepare(
+    new Query("Run").addFilter("requestId", FilterOperator.EQUAL, requestId).addSort("runId")
+  ).asIterable.map(new Run(_))
+}
+
+class Run(e: Entity) {
+  def requestId = e.getProperty("requestId").toString
+  def rawXml = e.getProperty("rawXml").asInstanceOf[Text].getValue
+  def runId = e.getProperty("runId").toString
+  def url = e.getProperty("url").toString
+  def loadTime = e.getProperty("loadTime").toString
+  def requests = e.getProperty("requests").toString
+  def render = e.getProperty("render").toString
+  def fullyLoaded = e.getProperty("fullyLoaded").toString
+  def docTime = e.getProperty("docTime").toString
+  def rawDate = e.getProperty("rawDate").toString
+  def date = e.getProperty("date").toString
 }
 
 class Tests {
@@ -28,8 +46,12 @@ class Tests {
 
   def all= {
     "li" #> allTests.map(test =>
-      "a [href]" #> test.requestId &
-      "a *" #> test.rawXml
+      ".requestId *" #> test.requestId &
+      ".arun" #> (test.runs.map(run =>
+        ".domrender *" #> run.render &
+        ".loadtime *" #> run.loadTime &
+        ".runid *" #> run.runId 
+       ))
      )
   }
 }
